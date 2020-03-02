edf0e73b04ea5b422ab301deeefc96df7ed061e5
Peter Bedrosian <peter_bedrosian@yahoo.com>
2020-03-02 23:48:23 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -29,15 +29,15 @@ class Song
   end
 
   def self.genre_count
-    genre_count = {}
-    @@genres.each do |genre|
-      if genre_count[genre]
-        genre_count += 1
-      else
-        genre_count[genre] = 1
+      genre_count = {}
+      @@genres.each do |genre|
+        if genre_count[genre]
+          genre_count[genre] += 1
+        else
+          genre_count[genre] = 1
+        end
       end
+      genre_count
     end
-    genre_count
-  end
 
 end
